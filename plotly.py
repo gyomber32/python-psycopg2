a806cbeda7386ba931422a23200750009c218160
Attila Gyömbér <gyomberattila@gmail.com>
2018-11-21 01:44:50 +0100

    None type error fixed, "gui" added

--- plotly.py
+++ plotly.py
@@ -1,9 +1,12 @@
+import sys
 import csv
 import psycopg2
 import math
 from sklearn import linear_model
-
+import time
+import datetime
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 
 def tupleMaxValue(values):
@@ -15,8 +18,46 @@ def tupleMaxValue(values):
     return max
 
 
-itemid = 220050
+itemid = 0
+file = ''
+
+print "--------------------------"
+print "1 - testsuly"
+print "2 - testmagassag"
+print "3 - szisztoles vernyomas"
+print "4 - diasztoles vernyomas"
+print "5 - vercukorszint"
+print "6 - veroxigenszint"
+print "--------------------------"
+print "Adjon meg egy meres tipust: "
+input = raw_input()
+
+print "Minimum hany meres: "
+minMeasurements = raw_input()
+print "Maximum hany meres: "
+maxMeasurements = raw_input()
+
+if int(input) == 1:
+    itemid = 226512
+    file = 'weight.csv'
+elif int(input) == 2:
+    itemid = 226730
+    file = 'height.csv'
+elif int(input) == 3:
+    itemid = 220050
+    file = 'systolic.csv'
+elif int(input) == 4:
+    itemid = 220051
+    file = 'diastolic.csv'
+elif int(input) == 5:
+    itemid = 1529
+    file = 'bloodGlucose.csv'
+elif int(input) == 6:
+    itemid = 220277
+    file = 'bloodOxygen.csv'
+
 print "Script started."
+print file
 # connection = None
 try:
     connection = psycopg2.connect(
@@ -24,19 +65,25 @@ try:
     cursor = connection.cursor()
     print "Connected to the database."
 
-    with open('systolic.csv', 'rb') as csvfile:
+    with open(file, 'rb') as csvfile:
         reader = csv.DictReader(csvfile)
         print "CSV file opened."
         counter = 0
         for row in reader:
             numberOfMeasurements = row['numberOfMeasurements']
             subject_id = row['subject_id']
-            minValue = 100
-            maxValue = 150
+            minValue = int(minMeasurements)
+            maxValue = int(maxMeasurements)
             if int(numberOfMeasurements) >= minValue and int(numberOfMeasurements) <= maxValue:
                 cursor.execute(
-                "SELECT valuenum FROM result WHERE subject_id = %s AND itemid = %s ORDER BY charttime;", (subject_id, itemid))
-                values = cursor.fetchall()
+                    "SELECT valuenum FROM result WHERE subject_id = %s AND itemid = %s ORDER BY charttime;", (subject_id, itemid))
+                selectedValues = cursor.fetchall()
+                values = []
+                for i in range(len(selectedValues)):
+                    if selectedValues[i] == (None,):
+                        values.append((0.0,))
+                    else:
+                        values.append(selectedValues[i])
                 train = []
                 for i in range(len(values)):
                     train.append(i)
@@ -56,18 +103,27 @@ try:
                 dispersionSquare = (float(temp)) / n
                 dispersion = math.sqrt(dispersionSquare)
                 print "Szoras: ", dispersion
-        
+
                 regr = linear_model.LinearRegression()
                 regr.fit(values, train)
                 prediction = regr.predict(values)
                 plt.plot(range(1, n + 1, 1), values, 'ro',
                          range(1, n + 1, 1), values, 'k')
-                        
+
                 plt.plot(prediction, values, color='blue', linewidth=3)
                 plt.ylabel('Mert ertek')
                 plt.xlabel('Meres sorszam')
-                plt.xticks(range(1, n + 1))
-                    
+                cursor.execute(
+                    "SELECT charttime FROM result WHERE subject_id = %s AND itemid = %s ORDER BY charttime;", (subject_id, itemid))
+                times = cursor.fetchall()
+                # print "Once upon the time: ", str(times[0])
+                # date = datetime.datetime(times[0], "%Y-%m-%d %H:%M")
+                # dates = time.mktime()
+                # print "Dates: ", date
+                # plt.plot(times)
+                # plt.xticks(range(0, n+1 , 1))
+                plt.xticks()
+
                 max = tupleMaxValue(values)
                 print "Legnagyobb ertek: ", max
                 plt.yticks(range(0, max+1, 5))
@@ -75,8 +131,8 @@ try:
                 plt.show()
                 print "-----------------------------"
                 counter += 1
-                if (counter == 20):
-                    break;
+                if (counter == 10):
+                    break
 except(Exception, psycopg2.DatabaseError) as error:
     print(error)
 finally:
